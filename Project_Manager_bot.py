b66f11156d7496f2c596611cf781778407aa658a
Arpith Krishna R <arpithkrishnar@gmail.com>
2025-09-06 22:52:42 +0530

    Update Command (base level) List Command (base level)

--- Project_Manager_bot.py
+++ Project_Manager_bot.py
@@ -1,5 +1,4 @@
 import discord
-import random
 import asyncio
 
 from discord import Embed
@@ -40,7 +39,7 @@ sheet = client.open_by_key(sheet_id)
 #=======================================================================================================================
 # Commands
 #=======================================================================================================================
-@bot.command(name='Commit', aliases=["commit"], help="Commit a message to the Google Sheet.")
+@bot.command(name='Commit', aliases=["commit","cum","COMMIT"], help="Commit a message to the Google Sheet.")
 async def commit(ctx,*,Message: str):
     user = ctx.author
     message = ctx.message
@@ -62,34 +61,45 @@ async def commit(ctx,*,Message: str):
     sheet.sheet1.update_cell(table_row_number,5, Message)
     await ctx.send(f"Message committed to Google Sheet by {nickname} with Commit id {commit_id} .")
 
-@bot.command(name='Update', aliases=["UPDATE"], help="Commit a message to the Google Sheet.")
-async def update(ctx,id: str):
+@bot.command(name='Update', aliases=["update","UPDATE"])
+async def update(ctx,commit_id: str,*,Message: str):
     user = ctx.author
+    nickname = user.display_name
     message = ctx.message
     guild_id = message.guild.id
     channel_id = message.channel.id
     message_id = message.id
     message_link = f"https://discord.com/channels/{guild_id}/{channel_id}/{message_id}"
-    nickname = user.display_name  # This gets the server nickname
-    table_row_number = int(sheet.sheet1.acell('G2').value)
-    user_cell = sheet.sheet1.find(nickname.upper())
-    index_data = int(sheet.sheet1.cell(2,user_cell.col).value)
-    nickname = nickname.lower()
-    commit_id = nickname[:5]+'#'+str(index_data)
-    #sheet.sheet1.update_cell(row_number,1, commit_id)  #commit id
-    sheet.sheet1.update_cell(table_row_number,1, commit_id)
-    sheet.sheet1.update_cell(table_row_number,2, nickname)  #server nickname
+    if(commit_id.lower() == 'last'):
+        table_row_number = int(sheet.sheet1.acell('G2').value) -1
+        commit_id = sheet.sheet1.cell(table_row_number,1).value
+    else:
+        table_row_number = sheet.sheet1.find(commit_id).row 
     sheet.sheet1.update_cell(table_row_number,3, message_link)
     sheet.sheet1.update_cell(table_row_number,4, ctx.message.created_at.strftime("%Y-%m-%d %H:%M:%S"))
     sheet.sheet1.update_cell(table_row_number,5, Message)
-    await ctx.send(f"Message committed to Google Sheet by {nickname} with Commit id {commit_id} .")
+    await ctx.send(f"Message Updated in Google Sheet by {nickname} to Commit id {commit_id} .")
 
+@bot.command(name='List', aliases=["list","LIST"])
+async def list(ctx,n:str = "1"):
+    if '#' in n.lower():
+        commit_id = n
+        table_row_number = sheet.sheet1.find(commit_id).row 
+        await ctx.send(f"Commit id:{sheet.sheet1.cell(table_row_number,1).value}, Message:{sheet.sheet1.cell(table_row_number,5).value}")
+        return
+    n = int(n)
+    table_row_number = int(sheet.sheet1.acell('G2').value)
+    if n > table_row_number -2:
+        await ctx.send(f"Only {table_row_number -2} commits are available.")
+        return
+    if n <= 0:
+        await ctx.send("Please provide a positive integer for the number of commits to list.")
+        return
+    await ctx.send(f"Last {n} commits are:")
+    for i in range(n):
+        table_row_number -= 1
+        await ctx.send(f"Commit id:{sheet.sheet1.cell(table_row_number,1).value}, Message:{sheet.sheet1.cell(table_row_number,5).value}")
+
+    
 
 bot.run(TOKEN)
-
-'''Completed till commit updates. 
-To do:
-- Update
-- List
-- Dynamic users
-'''
